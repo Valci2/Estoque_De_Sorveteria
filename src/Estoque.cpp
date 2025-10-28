4d0b9500d7d1e02bc0dd9c09022ca7ed48d27502
Bruno Melo <brunosouzal.melo@hotmail.com>
2025-10-28 00:54:33 -0300

    Update Estoque.cpp

--- src/Estoque.cpp
+++ src/Estoque.cpp
@@ -1,5 +1,57 @@
 #include "Estoque.h"
 #include <string>
+#include <fstream>  /* Para o .csv */
+#include <sstream>
+
+Estoque::Estoque() {
+    carregarDoCSV("estoque.csv");
+}
+
+void Estoque::carregarDoCSV(const std::string& nomeArquivo) {
+    std::ifstream arquivo(nomeArquivo);
+
+    if (!arquivo.is_open()) {
+        return;
+    }
+
+    std::string linha;
+
+    std::getline(arquivo, linha);
+
+    while (std::getline(arquivo, linha)) {
+        if (linha.empty()) {
+            continue;
+        }
+
+        std::stringstream ss(linha);
+        std::string pedaco;
+
+        std::string pedaco_id, nome, marca, sabor, data, pedaco_preco, pedaco_qtd, codigo;
+        double preco = 0.0;
+        int quantidade = 0;
+
+        std::getline(ss, pedaco_id, ';');
+        std::getline(ss, nome, ';');
+        std::getline(ss, marca, ';');
+        std::getline(ss, sabor, ';');
+        std::getline(ss, data, ';');
+        std::getline(ss, pedaco_preco, ';');
+        std::getline(ss, pedaco_qtd, ';');
+        std::getline(ss, codigo, ';');
+
+        try {
+            preco = std::stod(pedaco_preco);
+            quantidade = std::stoi(pedaco_qtd);
+        } catch (const std::exception& e) {
+            continue;
+        }
+
+        adicionar_produto(nome, marca, sabor, data, preco, quantidade);
+    }
+
+    arquivo.close();
+}
+
 
 bool Estoque::adicionar_produto(std::string nome, std::string marca, std::string sabor, std::string data_de_validade, double preco, int quantidade) {
 
@@ -39,7 +91,7 @@ bool Estoque::remover_produto(int id) {
 
     produtos_em_estoque.erase(produtos_em_estoque.begin() + id);
 
-    // Reatribui IDs (sua lógica original)
+    // Reatribui IDs
     for (size_t i = id; i < produtos_em_estoque.size(); i++) {
     // Apaga o produto
         produtos_em_estoque[i].set_id(static_cast<int>(i + 1));
@@ -69,4 +121,23 @@ bool Estoque::codigo_existe(const std::string& codigo) {
 // Retorna todos os produtos (para a Interface ler e imprimir)
 std::vector<Produto> Estoque::get_todos_os_produtos() {
     return produtos_em_estoque;
+}
+
+/* Para salvar em .csv */
+void Estoque::salvarParaCSV(const std::string& nomeArquivo) {
+    std::ofstream arquivo_csv(nomeArquivo);
+
+    arquivo_csv << "ID;Nome;Marca;Sabor;DataDeValidade;Preco;Quantidade;CodigoDoProduto" << std::endl;
+    
+    for (Produto& p : produtos_em_estoque) {
+        arquivo_csv << p.get_id() << ";"
+                    << p.get_nome() << ";"
+                    << p.get_marca() << ";"
+                    << p.get_sabor() << ";"
+                    << p.get_data_de_validade() << ";"
+                    << p.get_preco() << ";"
+                    << p.get_quantidade() << ";"
+                    << p.get_codigo_do_produto() << std::endl;
+    }
+    arquivo_csv.close();
 }
