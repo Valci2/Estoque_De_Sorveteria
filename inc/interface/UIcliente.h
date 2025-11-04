14aa4f1aefacc2f2539ce92cbce0512a1e69a9d9
Bruno Melo <brunosouzal.melo@hotmail.com>
2025-11-04 04:41:18 -0300

    Update UIcliente.h

--- inc/interface/UIcliente.h
+++ inc/interface/UIcliente.h
@@ -1,6 +1,28 @@
 #pragma once
 
+#include "negocio/Estoque.h"
+#include "negocio/Produto.h"
+#include <string>
+
 class UIcliente {
+private:
+    Estoque& meuEstoque;
+    std::string usuarioLogado;
+
+    // Funções do menu de login
+    void executarCriarConta();
+    std::string executarLogin(); 
+
+    // Funções do menu principal
+    void executarListarEstoque();
+    void executarSolicitarReserva();
+
+    void limpar_terminal();
+
+public:
+    UIcliente(Estoque& estoque);
+
+    // Menus
+    void mostrarMenuLogin();
+    void mostrarMenuPrincipal();
 };
-
-
