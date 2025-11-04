399ca12baea72730bc201ad6152fe0f0a715cff1
Bruno Melo <brunosouzal.melo@hotmail.com>
2025-11-04 04:46:56 -0300

    Update User.cpp

--- src/negocios/User.cpp
+++ src/negocios/User.cpp
@@ -9,3 +9,10 @@ bool User::logar() {
 
 int User::get_id() { return id; }
 std::string User::get_nome() { return nome; }
+
+void User::set_id(int id) {
+    this->id = id;  /* Permite que cliente altere o id */
+}
+void User::set_nome(const std::string& nome) {
+    this->nome = nome; /* Permite que o cliente altere o nome */
+}
