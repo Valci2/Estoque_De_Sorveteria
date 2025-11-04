6aeb36e85f45bcb70ac7648f9f13e6dfddee4ab5
Bruno Melo <brunosouzal.melo@hotmail.com>
2025-11-04 04:41:52 -0300

    Update Cliente.h

--- inc/negocio/Cliente.h
+++ inc/negocio/Cliente.h
@@ -1,5 +1,26 @@
-#include "User.h"
+#pragma once
+#include "negocio/User.h"
+#include <string>
 
-class Cliente : public User {
+class Cliente : public User { 
+private:
+    std::string dataDeNascimento;
+    std::string senha;
+    std::string email;
+
 public:
+    Cliente() = default;
+
+    Cliente(int id, std::string &nome);
+
+    std::string get_dataDeNascimento();
+    void set_dataDeNascimento(std::string &dataNasc);
+
+    void set_senha(const std::string &senha);
+    std::string get_senha();
+
+    void set_email(const std::string &email);
+    std::string get_email();
+
+    void criar() override; 
 };
